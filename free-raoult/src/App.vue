199060aa25bdc6b9b4adddf74836ce8c3da55372
Nicolas Zona <zonanico1@ferrero.com>
2022-12-15 17:41:57 +0100

    feat(core): General Progress

--- free-raoult/src/App.vue
+++ free-raoult/src/App.vue
@@ -1,21 +1,11 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
     <div class="bg-bluegray-900 text-gray-100 p-3 flex justify-content-between lg:justify-content-center align-items-center flex-wrap">
         <div class="font-bold mr-8">🔥 Libérez Raoult!</div>
-        <div class="align-items-center hidden lg:flex">
-            <span class="line-height-3">Le site sera bientot disponible.</span>
-        </div>
-        <a class="flex align-items-center ml-2 mr-8">
-            <a class="text-white" href="#"><span class="underline font-bold">En savoir plus</span></a>
-        </a>
-        <a v-ripple class="flex align-items-center no-underline justify-content-center border-circle text-gray-50 hover:bg-bluegray-700 cursor-pointer transition-colors transition-duration-150 p-ripple" style="width:2rem; height: 2rem">
-            <i class="pi pi-times"></i>
-        </a>
     </div>
   </header>
 
--- free-raoult/src/main.js
+++ free-raoult/src/main.js
@@ -3,13 +3,19 @@ import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config';
 import DataView from 'primevue/dataview';
+import Dropdown from 'primevue/dropdown';
+import Button from 'primevue/button';
 
 import './assets/main.css'
-
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+import 'primevue/resources/themes/mdc-dark-deeppurple/theme.css'
 const app = createApp(App)
+app.component('DataView', DataView)
+app.component('Dropdown', Dropdown)
+app.component('Button', Button)
 
 app.use(PrimeVue)
-app.component('DataView', DataView)
 app.use(router)
 
 app.mount('#app')
